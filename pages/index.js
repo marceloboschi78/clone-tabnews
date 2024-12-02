4788b4d64316c622236f559902869f32cbb779c9
Marcelo Boschi <marceloboschi78@hotmail.com>
2024-12-02 14:50:52 -0300

    modifica a frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home(){
-    return <h1>Pagina inicial do clone TabNews</h1>
+    return <h1>Pagina inicial do clone TabNews
+        Esse curso.dev é poderoso, teste de CI/CD com vercel
+    </h1>
 };
 
 export default Home;
