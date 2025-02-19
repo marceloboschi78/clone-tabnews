02b5ca38328cbbfd9ec6ec522c22b5145b519f04
Marcelo Boschi <marceloboschi78@hotmail.com>
2025-02-19 13:35:06 -0300

    test: test only 'major version' of postgres database

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -13,7 +13,12 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.6");
+  const dbMajorVersion = responseBody.dependencies.database.version.substring(
+    0,
+    2,
+  );
+
+  expect(dbMajorVersion).toEqual("16");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
